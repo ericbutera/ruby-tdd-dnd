5ef07b31512d03333dc6bf63505047114d607b18
Eric Butera <eric.butera@gmail.com>
2021-03-28 12:19:12 -0400

    Add alignment
    I'm not sure the idiomatic way to handle "enum" and getter/setters yet.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,18 @@
 class Character
     attr_reader :name
-    def initialize(name)
+    def initialize(name, alignment=:neutral)
         @name = name
+        @alignment = alignment
+    end
+
+    def alignment
+        @alignment 
+    end
+
+    def alignment=(alignment)
+        raise ArgumentError, "Invalid alignment <#{alignment}>" unless ALIGNMENTS.include? alignment
+        @alignment = alignment 
     end
 end
+
+ALIGNMENTS = [:good, :neutral, :evil]
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Character, "#name" do
         c = Character.new("eric")
         expect(c.name).to eq 'eric'
     end
+
+    it "default alignment is neutral" do
+        c = Character.new("luna")
+        expect(c.alignment).to eq :neutral
+    end
+
+    it "can set alignment to good" do
+        c = Character.new("luna")
+        c.alignment = :good
+        expect(c.alignment).to eq :good
+    end
+
 end
