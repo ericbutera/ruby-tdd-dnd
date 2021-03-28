98d9de83bd49cde3a46bb9ec32e6b62e28c4ed74
Eric Butera <eric.butera@gmail.com>
2021-03-28 13:10:54 -0400

    Alignment and Combat
    - alignments are Good, Evil, and Neutral
    - Feature: Character Can Attack

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,21 @@
+##
+# The hero in a story
 class Character
-    attr_reader :name
+    attr_reader :name, :armor_class, :hit_points
+
+    DEFAULT_ARMOR_CLASS = 10
+    DEFAULT_HIT_POINTS = 5
+
+    ##
+    # Create a new hero
+    # Params:
+    # +name+ Hero name
+    # +alignment+ An alignment- defaults to :neutral
     def initialize(name, alignment=:neutral)
         @name = name
         @alignment = alignment
+        @armor_class = DEFAULT_ARMOR_CLASS
+        @hit_points = DEFAULT_HIT_POINTS
     end
 
     def alignment
@@ -16,3 +29,23 @@ class Character
 end
 
 ALIGNMENTS = [:good, :neutral, :evil]
+
+
+##
+# Define rules of combat
+class Combat
+    ##
+    # Magic roll 20 always hits
+    ROLL_ALWAYS_HITS = 20
+
+    ##
+    # When an attack happens, is it a hit?
+    # +armor_class+ Character's armor class
+    # +roll+ the die roll
+    def hit?(armor_class, roll)
+        is_always_hit = roll == ROLL_ALWAYS_HITS
+        is_roll_above_ac = roll >= armor_class
+
+        is_always_hit || is_roll_above_ac
+    end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,6 @@
 require 'game'
 
-RSpec.describe Character, "#name" do
+RSpec.describe Character do
     it "character has a name" do
         c = Character.new("eric")
         expect(c.name).to eq 'eric'
@@ -17,4 +17,55 @@ RSpec.describe Character, "#name" do
         expect(c.alignment).to eq :good
     end
 
+    it "alignments are good, evil, neutral" do
+        c = Character.new("luna")
+        expect { 
+            c.alignment = "invalid-alignment-that-can-never-be" 
+        }.to raise_error(ArgumentError)
+    end
+
+    it "has 10 armor class" do
+        c = Character.new('luna')
+        expect(c.armor_class).to eq 10
+    end
+
+    it "has 5 hit points" do
+        c = Character.new('luna')
+        expect(c.hit_points).to eq 5
+    end
 end
+
+RSpec.describe Combat do
+    it "meet or beat opponents armor is a hit" do
+        combat = Combat.new
+        hit = combat.hit?(armor_class=10, roll=11)
+        expect(hit).to be true
+    end
+
+    # todo fence post issues
+
+    it "roll less than armor class misses" do
+        combat = Combat.new
+        hit = combat.hit?(armor_class=10, roll=9)
+        expect(hit).to be false
+    end
+
+    it "roll of 20 always hits" do
+        combat = Combat.new
+        hit = combat.hit?(30, 20)
+        expect(hit).to be true
+    end
+end
+
+
+# https://relishapp.com/rspec/rspec-expectations/v/3-10/docs/built-in-matchers/be-matchers
+
+# roll a 20 sided die
+# roll must meet or beat opponents armor class to hit
+# a natural roll of 20 always hits
+
+#class Die
+#    def roll
+#        rand(1..20)
+#    end
+#end
